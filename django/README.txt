625a1b855aa7620d10e6f8d0b12b5098e4cc71c1
0TShadowater0 <igorbgl1@hotmail.com>
2019-06-23 23:43:44 -0300

    Finish blog assessment.

---
+++ django/README.txt
@@ -0,0 +1,9 @@
+É necessário fazer login para ver e adicionar comentários
+
+Para fazer login em admin utilize as credenciais
+username: quarteto
+password: fantastico
+
+Para fazer login em um usuário de teste no site principal utilize as credenciais
+username: testuser
+password: woodgrecia
--- django/diyblog/blog/views.py
+++ django/diyblog/blog/views.py
@@ -1,3 +1,75 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+from blog.models import Blog, BlogAuthor, BlogComment
+from django.contrib.auth.models import User
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_blogs = Blog.objects.all().count()
+    
+    # The 'all()' is implied by default.    
+    num_authors = BlogAuthor.objects.count()
+    
+    context = {
+        'num_blogs': num_blogs,
+        'num_authors': num_authors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BlogListView(generic.ListView):
+    model = Blog
+    paginate_by = 5
+
+class BlogDetailView(generic.DetailView):
+    model = Blog
+
+class BlogListbyAuthorView(generic.ListView):
+    model = Blog
+    paginate_by = 5
+    template_name ='blog/blog_list_by_author.html'
+    
+    def get_queryset(self):
+        id = self.kwargs['pk']
+        target_author=get_object_or_404(BlogAuthor, pk = id)
+        return Blog.objects.filter(author=target_author)
+        
+    def get_context_data(self, **kwargs):
+        context = super(BlogListbyAuthorView, self).get_context_data(**kwargs)
+        context['blogger'] = get_object_or_404(BlogAuthor, pk = self.kwargs['pk'])
+        return context
+
+class BloggerListView(generic.ListView):
+    model = BlogAuthor
+    paginate_by = 5
+
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.urls import reverse
+
+
+class BlogCommentCreate(LoginRequiredMixin, CreateView):
+    model = BlogComment
+    fields = ['description',]
+
+    def get_context_data(self, **kwargs):
+        context = super(BlogCommentCreate, self).get_context_data(**kwargs)
+        context['blog'] = get_object_or_404(Blog, pk = self.kwargs['pk'])
+        return context
+        
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.blog=get_object_or_404(Blog, pk = self.kwargs['pk'])
+        return super(BlogCommentCreate, self).form_valid(form)
+
+    def get_success_url(self): 
+        return reverse('blog-detail', kwargs={'pk': self.kwargs['pk'],})
